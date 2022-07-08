3981210f3ab75c596d7b07cd78428657eec26655
王涛 <1433953566@qq.com>
2022-07-08 12:33:23 +0800

    1

--- app/src/main/java/com/puyue/www/qiaoge/fragment/market/ProdAdapter.java
+++ app/src/main/java/com/puyue/www/qiaoge/fragment/market/ProdAdapter.java
@@ -52,56 +52,56 @@ public class ProdAdapter extends BaseQuickAdapter<MarketRightModel.DataBean.Bran
             helper.getView(R.id.rl).setVisibility(View.GONE);
         }
 
-//        ViewGroup.LayoutParams lp = recyclerViewProd.getLayoutParams();
-//        lp.height = DensityUtil.dip2px(150 * 1,mContext);
+        ViewGroup.LayoutParams lp = recyclerViewProd.getLayoutParams();
+        lp.height = DensityUtil.dip2px(150 * 1,mContext);
 
-//        rl.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (list.size() > 3) {
-//                    if(open) {
-//                        lp.height = DensityUtil.dip2px(150 * 1,mContext);
-//                        open = false;
-//                        tv_expand.setText("展开");
-//                        recyclerViewProd.setLayoutManager(new GridLayoutManager(mContext,3));
-//                        prodInnerAdapter = new ProdInnerAdapter(R.layout.item_prod_inner,list);
-//                        recyclerViewProd.setAdapter(prodInnerAdapter);
-//                    }else {
-//                        if(list.size()%3==0) {
-//                            lp.height = DensityUtil.dip2px(150 *(list.size()/3),mContext);
-//                            tv_expand.setText("收起");
-//                            open = true;
-//                            recyclerViewProd.setLayoutManager(new GridLayoutManager(mContext,3));
-//                            prodInnerAdapter = new ProdInnerAdapter(R.layout.item_prod_inner,list);
-//                            recyclerViewProd.setAdapter(prodInnerAdapter);
-//
-//                        }else if(list.size()%3==1) {
-//                            lp.height = DensityUtil.dip2px(150 *(list.size()/2),mContext);
-//
-//                            tv_expand.setText("收起");
-//                            open = true;
-//                            recyclerViewProd.setLayoutManager(new GridLayoutManager(mContext,3));
-//                            prodInnerAdapter = new ProdInnerAdapter(R.layout.item_prod_inner,list);
-//                            recyclerViewProd.setAdapter(prodInnerAdapter);
-//
-//                        }else if(list.size()%3==2) {
-//                            lp.height = DensityUtil.dip2px(150 *(list.size()/2),mContext);
-//                            tv_expand.setText("收起");
-//                            open = true;
-//                            recyclerViewProd.setLayoutManager(new GridLayoutManager(mContext,3));
-//                            prodInnerAdapter = new ProdInnerAdapter(R.layout.item_prod_inner,list);
-//                            recyclerViewProd.setAdapter(prodInnerAdapter);
-//                        }
-//                    }
-//                } else {
-//                    lp.height = DensityUtil.dip2px(150,mContext);
-//                    recyclerViewProd.setLayoutManager(new GridLayoutManager(mContext,3));
-//                    prodInnerAdapter = new ProdInnerAdapter(R.layout.item_prod_inner,list);
-//                    recyclerViewProd.setAdapter(prodInnerAdapter);
-//                }
-//            }
-//        });
-//        recyclerViewProd.setLayoutParams(lp);
+        rl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (list.size() > 3) {
+                    if(open) {
+                        lp.height = DensityUtil.dip2px(150 * 1,mContext);
+                        open = false;
+                        tv_expand.setText("展开");
+                        recyclerViewProd.setLayoutManager(new GridLayoutManager(mContext,3));
+                        prodInnerAdapter = new ProdInnerAdapter(R.layout.item_prod_inner,list);
+                        recyclerViewProd.setAdapter(prodInnerAdapter);
+                    }else {
+                        if(list.size()%3==0) {
+                            lp.height = DensityUtil.dip2px(150 *(list.size()/3),mContext);
+                            tv_expand.setText("收起");
+                            open = true;
+                            recyclerViewProd.setLayoutManager(new GridLayoutManager(mContext,3));
+                            prodInnerAdapter = new ProdInnerAdapter(R.layout.item_prod_inner,list);
+                            recyclerViewProd.setAdapter(prodInnerAdapter);
+
+                        }else if(list.size()%3==1) {
+                            lp.height = DensityUtil.dip2px(150 *(list.size()/2),mContext);
+
+                            tv_expand.setText("收起");
+                            open = true;
+                            recyclerViewProd.setLayoutManager(new GridLayoutManager(mContext,3));
+                            prodInnerAdapter = new ProdInnerAdapter(R.layout.item_prod_inner,list);
+                            recyclerViewProd.setAdapter(prodInnerAdapter);
+
+                        }else if(list.size()%3==2) {
+                            lp.height = DensityUtil.dip2px(150 *(list.size()/2),mContext);
+                            tv_expand.setText("收起");
+                            open = true;
+                            recyclerViewProd.setLayoutManager(new GridLayoutManager(mContext,3));
+                            prodInnerAdapter = new ProdInnerAdapter(R.layout.item_prod_inner,list);
+                            recyclerViewProd.setAdapter(prodInnerAdapter);
+                        }
+                    }
+                } else {
+                    lp.height = DensityUtil.dip2px(150,mContext);
+                    recyclerViewProd.setLayoutManager(new GridLayoutManager(mContext,3));
+                    prodInnerAdapter = new ProdInnerAdapter(R.layout.item_prod_inner,list);
+                    recyclerViewProd.setAdapter(prodInnerAdapter);
+                }
+            }
+        });
+        recyclerViewProd.setLayoutParams(lp);
 
     }
 }
