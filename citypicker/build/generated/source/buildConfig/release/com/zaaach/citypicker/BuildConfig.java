17b25d06b46d36d48ea333da96c882281752ce1c
王涛 <1433953566@qq.com>
2022-11-16 08:58:25 +0800

    1

---
+++ citypicker/build/generated/source/buildConfig/release/com/zaaach/citypicker/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zaaach.citypicker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.zaaach.citypicker";
+  public static final String BUILD_TYPE = "release";
+}
